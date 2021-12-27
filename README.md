a76c23db758f1c520dcb203e263f12c2eadfb1be
Steve Galbraith <galbernator@gmail.com>
2021-12-27 13:21:47 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftUIProtocols
+
+A description of this package.
---
+++ Sources/SwiftUIProtocols/SwiftUIProtocols.swift
@@ -0,0 +1,6 @@
+public struct SwiftUIProtocols {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
