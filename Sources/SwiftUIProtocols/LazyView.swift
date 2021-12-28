879bd32c9d7577b88da63e3f047ad0f50454403f
Steve Galbraith <galbernator@gmail.com>
2021-12-28 13:36:14 -0700

    Add LazyView

---
+++ Sources/SwiftUIProtocols/LazyView.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Steve Galbraith on 12/28/21.
+//
+
+import SwiftUI
+
+/// Allows for the view body to be specifically built when the body is called
+public struct LazyView<Content: View>: View {
+    @ViewBuilder var build: (() -> Content)
+
+    public var body: some View {
+        build()
+    }
+}
+
+struct LazyView_Previews: PreviewProvider {
+    static var previews: some View {
+        LazyView {
+            Text("Testing...")
+        }
+    }
+}
